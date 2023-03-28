7d0a315bc79db7784206b692b55b290a16e80134
Nazar Sinilov <nsinilov.itrum@gmail.com>
2023-03-28 17:05:32 +0300

    complete

---
+++ task-3/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Modal} from './Modal/Modal';
+ 
+function App() {
+  return (
+    <div >
+      <Modal text='Close'/>
+    </div>
+  );
+}
+
+export default App;

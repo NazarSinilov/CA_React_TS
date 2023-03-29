d370ee062832ca49e8a146ba507159f91c5a52da
NazarSinilov <nsinilov.itrum@gmail.com>
2023-03-29 18:36:06 +0300

    complete

--- task-7/src/App.tsx
+++ task-7/src/App.tsx
@@ -1,43 +1,11 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, NavLink, Route, Routes} from "react-router-dom";
-import Message from "./pages/Contact";
-import Main from "./pages/Home";
-import {CSSTransition} from 'react-transition-group';
 
 function App() {
-  const routes = [
-    {path: '/', Component: Main},
-    {path: '/contact', Component: Message},
-  ]
-
   return (
-    <BrowserRouter>
-      <div className="App">
-        <div className="div">
-          <NavLink to='/'>Main</NavLink>
-          <NavLink to='/contact'>Contact</NavLink>
-        </div>
-        <Routes>
-          {routes.map(({path, Component}) =>
-            <Route key={path} path={path}>
-              {({match}) => {
-                return <CSSTransition
-                  timeout={1000}
-                  classNames='page'
-                  unmountOnExit
-                  in={match != null}
-                >
-                  <Component />
-                </CSSTransition>
-              }
-              }
-            </Route>
-          )}
-        </Routes>
+    <div>
 
-      </div>
-    </BrowserRouter>
+    </div>
   );
 }
 
--- task-7/src/pages/Home.tsx
+++ task-7/src/pages/Home.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const Home = () => {
-    return (
-        <div className='main'>
-            <h1>Главная</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium blanditiis consequuntur corporis cupiditate distinctio eaque esse et eveniet inventore laboriosam laborum laudantium minus nisi nobis, quam, repellat saepe, tenetur vero!</p>
-        </div>
-    );
+  return (
+    <div className='main'>
+      <h1>Главная</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium blanditiis consequuntur corporis cupiditate distinctio eaque esse et eveniet inventore laboriosam laborum laudantium minus nisi nobis, quam, repellat saepe, tenetur vero!</p>
+    </div>
+  );
 };
 
 export default Home;

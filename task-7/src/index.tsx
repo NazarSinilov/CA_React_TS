cb02c2e4bc90f9a26c15ff63ca4fa4466aeac565
NazarSinilov <nsinilov.itrum@gmail.com>
2023-03-29 18:37:04 +0300

    complete

--- task-7/src/index.tsx
+++ task-7/src/index.tsx
@@ -1,13 +1,89 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {createRef} from 'react';
+import {createRoot} from 'react-dom/client';
 import './index.css';
-import App from './App';
+import './App.css';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import {createBrowserRouter, Link, RouterProvider, useLocation, useOutlet} from 'react-router-dom';
+import {SwitchTransition, CSSTransition} from 'react-transition-group';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+interface RoutesI {
+  path: string,
+  name: string,
+  element: JSX.Element,
+  nodeRef: any
+}
+
+const routes: RoutesI[] = [
+  {
+    path: "/",
+    name: 'Main',
+    element: <Home/>,
+    nodeRef: createRef(),
+  },
+  {
+    path: "/about",
+    name: 'About',
+    element: <Contact/>,
+    nodeRef: createRef(),
+  },
+];
+
+const navArr = [
+  {
+    path: '/',
+    text: 'Main'
+  },
+  {
+    path: '/about',
+    text: 'About'
+  }
+]
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Example/>,
+    children: routes.map((route) => ({
+      index: route.path === '/',
+      path: route.path === '/' ? undefined : route.path,
+      element: route.element,
+    })),
+  }
+])
+
+function Example() {
+  const location = useLocation();
+  const currentOutlet = useOutlet();
+
+  const {nodeRef} = routes.find((route) => route.path === location.pathname) ?? {}
+
+  return (
+    <div>
+      {navArr.map(item => {
+        return <Link to={item.path} key={item.path} className='item'>{item.text}</Link>
+      })}
+      <div>
+        <SwitchTransition>
+          <CSSTransition
+            key={location.pathname}
+            nodeRef={nodeRef}
+            timeout={300}
+            classNames="page"
+            unmountOnExit
+          >
+            {(state) => (
+              <div ref={nodeRef} className="page">
+                {currentOutlet}
+              </div>
+            )}
+          </CSSTransition>
+        </SwitchTransition>
+      </div>
+    </div>
+  );
+};
+
+const container = document.getElementById('root') as Element | DocumentFragment;
+const root = createRoot(container)
+root.render(<RouterProvider router={router}/>)
